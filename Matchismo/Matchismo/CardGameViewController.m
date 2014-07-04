c4b75754b68778cc70774dc37a125e7b87e97c43
onthehardestway <onthehardestway@gmail.com>
2014-07-04 09:18:08 +0800

    Assignment 2: add reset button

--- Matchismo/Matchismo/CardGameViewController.m
+++ Matchismo/Matchismo/CardGameViewController.m
@@ -10,7 +10,7 @@
 #import "PlayingCardDeck.h"
 #import "CardMatchingGame.h"
 
-@interface CardGameViewController ()
+@interface CardGameViewController () <UIAlertViewDelegate>
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
 @property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 
@@ -19,17 +19,47 @@
 
 @implementation CardGameViewController
 
+- (Deck *)createDeck
+{
+    return [[PlayingCardDeck alloc] init];
+}
+
+- (CardMatchingGame *)generateNewGame
+{
+    return [[CardMatchingGame alloc] initWithCardCount:[self.cardButtons count] usingDeck:[self createDeck]];
+}
+
 - (CardMatchingGame *)game
 {
     if (_game == nil) {
-        _game = [[CardMatchingGame alloc] initWithCardCount:[self.cardButtons count] usingDeck:[self createDeck]];
+        _game = [self generateNewGame];
     }
     return _game;
 }
 
-- (Deck *)createDeck
+- (void)startNewGame
 {
-    return [[PlayingCardDeck alloc] init];
+    self.game = [self generateNewGame];
+    [self updateUI];
+}
+
+- (IBAction)touchResetButton:(UIButton *)sender
+{
+    // according to HIG:
+    // When the most likely button performs a destructive action, it should be on the left in a two-button alert. The button that cancels this action should be on the right.
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"New Game", @"New Game")
+                                                        message:NSLocalizedString(@"Do you want to start a new game?", @"Do you want to start a new game?")
+                                                       delegate:self
+                                              cancelButtonTitle:nil
+                                              otherButtonTitles:NSLocalizedString(@"OK", @"OK"), NSLocalizedString(@"Cancel", @"Cancel"), nil];
+    [alertView show];
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex == 0) {
+        [self startNewGame];
+    }
 }
 
 - (IBAction)touchCardButton:(UIButton *)sender
